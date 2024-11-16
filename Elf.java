318cc2cf7e7bf5a55a5e85b9981d32465ddc3501
NJWeb.Solutions <david@njweb.solutions>
2024-11-15 19:48:12 -0500

    constructor

--- Elf.java
+++ Elf.java
@@ -3,4 +3,12 @@ public class Elf extends Creature {
     private static final int MIN_ELF_HP = 8;
     private static final int MAX_ELF_STR = 18;
     private static final int MIN_ELF_STR = 5;
+    
+    public Elf() {
+        super(
+            Randomizer.nextInt(MAX_ELF_STR - MIN_ELF_STR + 1) + MIN_ELF_STR,
+            Randomizer.nextInt(MAX_ELF_HP - MIN_ELF_HP + 1) + MIN_ELF_HP
+        );
+}
+
 }
