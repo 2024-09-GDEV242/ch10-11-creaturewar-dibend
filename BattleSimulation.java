4b9a194d15b528df0c613a0dc48e6aaf82604f34
NJWeb.Solutions <david@njweb.solutions>
2024-11-17 16:39:43 -0500

    Create BattleSimulation class

---
+++ BattleSimulation.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class BattleSimulation {
+ private ArrayList<Creature> goodArmy;
+ private ArrayList<Creature> evilArmy;
+ private Random random = new Random();
+}
